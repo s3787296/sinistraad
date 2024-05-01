7d2ef7f79c9f192c1e3e97636ac170ae7c479933
Solus <lucidnoir@outlook.com>
2024-05-01 21:03:48 +1000

    Add files via upload
    merging main map updates with local version and adding local version updates

---
+++ Combat.java
@@ -0,0 +1,110 @@
+public class Combat {
+
+    // random encounters
+    public static String[] encounters = { "Battle", "Battle", "Battle", "Rest" };
+
+    // enemy names
+    public static String[] enemies = { "Enemy1", "Enemy2", "Enemy3", "Enemy4" };
+
+    // main battle method
+    public static void battle(Enemy enemy) {
+        // turn tracker variable
+        int turn = 1;
+        // main battle loop
+        while (true) {
+            Game.clearConsole();
+            Game.printHeading(enemy.name + "\nHP: " + enemy.curHp + "/" + enemy.maxHp);
+            Game.printHeading(Game.player.name + "\nHP: " + Game.player.curHp + "/" + Game.player.maxHp);
+            Game.printSeperator(20);
+            System.out.println("[1] Attack\n[2] Dodge\n[3] Item");
+            int input = Game.readInt();
+            // reacting to player input
+            if (input == 1) {
+                // attack
+                int dmgDealt;
+                int dmgTaken;
+                // player damage dealt calculation
+                dmgDealt = Game.player.attack() - enemy.defence();
+                if (Game.player.attack() >= enemy.defence()) {
+                    dmgDealt = Game.player.attack() *2 - enemy.defence();
+                }else{
+                    dmgDealt = Game.player.attack() * Game.player.attack() / enemy.defence();
+                }
+                if (dmgDealt < 0) {
+                    dmgDealt = 0;
+                }
+                // player dmg taken calculation
+                if (enemy.attack() >= Game.player.defence()) {
+                    dmgTaken = enemy.attack() * 2 - Game.player.defence();
+                } else {
+                    dmgTaken = enemy.attack() * enemy.attack() / Game.player.defence();
+                }
+                if (dmgTaken < 0) {
+                    dmgTaken = 0;
+                }
+                // calc dmg to both parties
+                Game.player.curHp -= dmgTaken;
+                enemy.curHp -= dmgDealt;
+                // print the info of this turn
+                Game.clearConsole();
+                Game.printHeading("BATTLE");
+                Game.printHeading("Turn " + turn);
+                System.out.println("You dealt " + dmgDealt + " damage to the " + enemy.name + "!");
+                System.out.println("The " + enemy.name + " dealt " + dmgTaken + " damage to you!");
+                Game.continueKey();
+                turn++;
+                // check if player died
+                if (Game.player.curHp <= 0) {
+                    Game.playerDied(); // method to end the game
+                    break;
+                } else if (enemy.curHp <= 0) {
+                    // tell the player they won
+                    Game.clearConsole();
+                    Game.printHeading("You defeated the " + enemy.name + "!");
+                    // increase player xp
+                    System.out.println("You earned " + enemy.xp + "XP!");
+                    Game.player.xp += enemy.xp;
+                    // random drops
+                    boolean addRest = (Math.random() * 5 + 1 <= 2.25);
+                    int goldEarnt = (int) (Math.random() * enemy.xp);
+
+                    if (addRest) {
+                        Game.player.rests++;
+                        System.out.println("You fought well and earned an additional rest.\nRests available: ["+ Game.player.rests + "]!");
+                    }
+                    if (goldEarnt > 0) {
+                        Game.player.gold += goldEarnt;
+                        System.out.println("You collect " + goldEarnt + " gold from the " + enemy.name + "'s corpse.");
+                    }
+                    Game.continueKey();
+                    break;
+                }
+            } else if (input == 2) {
+                // dodge
+            } else if (input == 3) {
+                // use potion
+                Game.clearConsole();
+                if (Game.player.potions > 0 && Game.player.curHp < Game.player.maxHp) {
+                    // player able to use a potion
+                    Game.printHeading("Do you want to use a potion? [" + Game.player.potions + "] left.");
+                    // confirm player wants to use a potion
+                    System.out.println("[1] Yes\n[2] No");
+                    input = Game.readInt();
+                    if (input == 1) {
+                        // player takes potion
+                        System.out.println("You used a potion and restored " + (Game.player.maxHp - Game.player.curHp)+ " health.");
+                        Game.player.curHp = Game.player.maxHp;
+                        Game.continueKey();
+                    } else if (input == 2) {
+                        System.out.println("You decided not to use a potion.");
+                        Game.continueKey();
+                    }
+                } else {
+                    // player unable to use a potion
+                    Game.printHeading("You can't do that right now.");
+                    Game.continueKey();
+                }
+            }
+        }
+    }
+}
