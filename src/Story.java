7e68338ff6e1757b6f5ef4c1de6e5a5fd224f922
tuesday <159415940+eutsday@users.noreply.github.com>
2024-05-06 15:59:33 +1000

    Update Story.java
    Made quick notes.

--- src/Story.java
+++ src/Story.java
@@ -1,3 +1,7 @@
 public class Story {
-  
+    // Pulls all the other things.
+        // E.g. Method: A1Story (Dialogue)
+        // Method: A1Map
+        // Method: A1Encounters
+        // Method: AvailableDirections
 }
