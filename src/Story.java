8852c55c5782c6f3946410a16a1a6881bbac5731
tuesday <159415940+eutsday@users.noreply.github.com>
2024-05-28 15:12:55 +1000

    Delete src/Story.java
    No longer required - content will go into Dialogue.java

--- src/Story.java
+++
@@ -1,8 +0,0 @@
-public class Story {
-        // Method: A1Name
-        // Method: A1Dialogue
-        // Method: A1Map
-        // Method: A1Encounters
-        // Method: A1Directions
-        // Method: A1Items
-}
