49287197e7354a41a742e81328f44827bf38180b
Solus <lucidnoir@outlook.com>
2024-05-30 14:23:18 +1000

    Add files via upload

--- src/LocationList.java
+++ src/LocationList.java
@@ -23,96 +23,96 @@ public class LocationList {
         // Creating and setting location objects at corresponding index value
         // nullLocation is used as a placeholder for index 0 in each ArrayList - makes
         // the other ArrayLists easier to work with
-        Location nullLocation = new Location("NULL", "NULL", "NULL", "NULL");
+        Location nullLocation = new Location("NULL", "NULL", "NULL");
         //
         // Column A
         aList.add(nullLocation);
         //
-        Location a1Location = new Location("A1", Map.a1Map(), "CASTLE ARMOURY", Story.a1Story(), EnemyList.forgemasterFuego());
+        Location a1Location = new Location("A1", "CASTLE ARMOURY", Story.a1Story(), EnemyList.forgemasterFuego());
         aList.add(a1Location);
         //
-        Location a2Location = new Location("A2", Map.a2Map(), "CASTLE INTERIOR", Story.a2Story(), EnemyList.stoneskinGargoyle());
+        Location a2Location = new Location("A2", "CASTLE INTERIOR", Story.a2Story(), EnemyList.stoneskinGargoyle());
         aList.add(a2Location);
         //
-        Location a3Location = new Location("A3", Map.a3Map(), "CASTLE COURTYARD", Story.a3Story(), EnemyList.gatekeeperSyek());
+        Location a3Location = new Location("A3", "CASTLE COURTYARD", Story.a3Story(), EnemyList.gatekeeperSyek());
         aList.add(a3Location);
         //
-        Location a4Location = new Location("A4", Map.a4Map(), "DUSKWOOD FOREST", Story.a4Story(), EnemyList.feralDuskwolf());
+        Location a4Location = new Location("A4", "DUSKWOOD FOREST", Story.a4Story(), EnemyList.feralDuskwolf());
         aList.add(a4Location);
         //
-        Location a5Location = new Location("A5", "NULL", "ENDLESS FOG - LOCATION NOT ACCESSIBLE", "Something has gone seriously wrong.");
+        Location a5Location = new Location("A5", "NOT ACCESSIBLE", "Something has gone wrong.");
         aList.add(a5Location);
         //
         // Column B
         bList.add(nullLocation);
         //
-        Location b1Location = new Location("B1", Map.b1Map(), "CASTLE INTERIOR", Story.b1Story(), EnemyList.stoneskinGargoyle());
+        Location b1Location = new Location("B1", "CASTLE INTERIOR", Story.b1Story(), EnemyList.stoneskinGargoyle());
         bList.add(b1Location);
         //
-        Location b2Location = new Location("B2", Map.b2Map(), "CASTLE INTERIOR", Story.b2Story(), EnemyList.stoneskinGargoyle());
+        Location b2Location = new Location("B2", "CASTLE INTERIOR", Story.b2Story(), EnemyList.stoneskinGargoyle());
         bList.add(b2Location);
         //
-        Location b3Location = new Location("B3", Map.b3Map(), "CASTLE INTERIOR", Story.b3Story(), EnemyList.animatedGuard());
+        Location b3Location = new Location("B3", "CASTLE INTERIOR", Story.b3Story(), EnemyList.animatedGuard());
         bList.add(b3Location);
         //
-        Location b4lLocation = new Location("B4", Map.b4Map(), "DUSKWOOD FOREST", Story.b4Story(), EnemyList.feralDuskwolf());
+        Location b4lLocation = new Location("B4", "DUSKWOOD FOREST", Story.b4Story(), EnemyList.feralDuskwolf());
         bList.add(b4lLocation);
         //
-        Location b5Location = new Location("B5", Map.b5Map(), "DUSKWOOD FOREST", Story.b5Story(), EnemyList.feralDuskwolf());
+        Location b5Location = new Location("B5", "DUSKWOOD FOREST", Story.b5Story(), EnemyList.feralDuskwolf());
         bList.add(b5Location);
         //
         // Column C
         cList.add(nullLocation);
         //
-        Location c1Location = new Location("C1", Map.c1Map(), "CASTLE CATHEDRAL", Story.c1Story());
+        Location c1Location = new Location("C1", "CASTLE CATHEDRAL", Story.c1Story(), EnemyList.animatedGuard());
         cList.add(c1Location);
         //
-        Location c2Location = new Location("C2", Map.c2Map(), "DUSKWOOD CLEARING", Story.c2Story());
+        Location c2Location = new Location("C2", "DUSKWOOD CLEARING", Story.c2Story());
         cList.add(c2Location);
         //
-        Location c3Location = new Location("C3", Map.c3Map(), "DUSKWOOD FOREST", Story.c3Story(), EnemyList.arachnidSpawn());
+        Location c3Location = new Location("C3", "DUSKWOOD FOREST", Story.c3Story(), EnemyList.arachnidSpawn());
         cList.add(c3Location);
         //
-        Location c4Location = new Location("C4", Map.c4Map(), "DUSKWOOD FOREST", Story.c4Story(), EnemyList.feralDuskwolf());
+        Location c4Location = new Location("C4", "DUSKWOOD FOREST", Story.c4Story(), EnemyList.feralDuskwolf());
         cList.add(c4Location);
         //
-        Location c5Location = new Location("C5", Map.c5Map(), "DUSKWOOD DEN",  Story.c5Story(), EnemyList.alphaDuskwolf());
+        Location c5Location = new Location("C5", "DUSKWOOD DEN", Story.c5Story(), EnemyList.alphaDuskwolf());
         cList.add(c5Location);
         //
         // Column D
         dList.add(nullLocation);
         //
-        Location d1Location = new Location("D1", Map.d1Map(), "CASTLE CHAMBERS", Story.d1Story(), EnemyList.animatedGuard());
+        Location d1Location = new Location("D1", "CASTLE CHAMBERS", Story.d1Story(), EnemyList.animatedGuard());
         dList.add(d1Location);
         //
-        Location d2Location = new Location("D2", Map.d2Map(), "MYSTIC LAKE", Story.d2Story(), EnemyList.lostMaiden());
+        Location d2Location = new Location("D2", "MYSTIC LAKE", Story.d2Story(), EnemyList.lostMaiden());
         dList.add(d2Location);
         //
-        Location d3Location = new Location("D3", Map.d3Map(), "DUSKWOOD FOREST", Story.d3Story(), EnemyList.arachnidSpawn());
+        Location d3Location = new Location("D3", "DUSKWOOD FOREST", Story.d3Story(), EnemyList.arachnidSpawn());
         dList.add(d3Location);
         //
-        Location d4Location = new Location("D4", Map.d4Map(), "DUSKWOOD FOREST", Story.d4Story(), EnemyList.arachnidSpawn());
+        Location d4Location = new Location("D4", "DUSKWOOD FOREST", Story.d4Story(), EnemyList.arachnidSpawn());
         dList.add(d4Location);
         //
-        Location d5Location = new Location("D5", Map.d5Map(), "DUSKWOOD FOREST", Story.d5Story(), EnemyList.arachnidSpawn());
+        Location d5Location = new Location("D5", "DUSKWOOD FOREST", Story.d5Story(), EnemyList.arachnidSpawn());
         dList.add(d5Location);
         //
         // Column E
         eList.add(nullLocation);
         //
-        Location e1Location = new Location("E1", Map.e1Map(), "THRONE ROOM", Story.e1Story(), EnemyList.countEripmav());
+        Location e1Location = new Location("E1", "THRONE ROOM", Story.e1Story(), EnemyList.countEripmav());
         eList.add(e1Location);
         //
-        Location e2Location = new Location("E2", "NULL", "TWISTED MOUNTAINS - LOCATION NOT ACCESSIBLE", "Something has gone seriously wrong.");
+        Location e2Location = new Location("E2", "NOT ACCESSIBLE", "Something has gone wrong.");
         eList.add(e2Location);
         //
-        Location e3Location = new Location("E3", Map.e3Map(), "TWISTED CAVE", Story.e3Story(), EnemyList.arachnidMatriarch());
+        Location e3Location = new Location("E3", "TWISTED CAVE", Story.e3Story(), EnemyList.arachnidMatriarch());
         eList.add(e3Location);
         //
-        Location e4Location = new Location("E4", Map.e4Map(), "TILLED PLAINS", Story.e4Story());
+        Location e4Location = new Location("E4", "TILLED PLAINS", Story.e4Story());
         eList.add(e4Location);
         //
-        Location e5Location = new Location("E5", Map.e5Map(), "OLD TOWN", Story.e5Story());
+        Location e5Location = new Location("E5", "OLD TOWN", Story.e5Story());
         eList.add(e5Location);
     }
 }
